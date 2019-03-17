820646882d4badcdd50cba871b177c7fea3e4c5d
aruyamam <aruyamam@gmail.com>
2019-03-17 11:56:34 +0900

    Posts Access Control

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -21,3 +21,13 @@ function flash(
       unset($_SESSION[$name . '_class']);
    }
 }
+
+function isLoggedIn()
+{
+   if (isset($_SESSION['user_id'])) {
+      return true;
+   }
+   else {
+      return false;
+   }
+}
